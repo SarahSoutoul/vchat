890ebfaf9ae60b34c0a42cf17f21392d8ce0fad5
tombyrnefp <tom.byrne@getfutureproof.co.uk>
2023-07-14 10:24:53 +0100

    Starter Code

--- chatgpt.js
+++ chatgpt.js
@@ -1,32 +1,7 @@
 require('dotenv').config();
 
 const chat = async (char, question) => {
-    return new Promise(async (resolve, reject) => {
-        const { Configuration, OpenAIApi } = require("openai");
-        const configuration = new Configuration({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
-
-        //Creates the OpenAI instance
-        const openai = new OpenAIApi(configuration);
-        
-        try 
-        {
-            const response = await await openai.createChatCompletion({
-                model:"gpt-3.5-turbo",
-                messages:[
-                    {"role": "system","content": `You are ${char} from Heathers the musical. You are to answer the question as this character. Do not break character. Please return your answer formatted with HTML including a <p> tag for new paragraphs.`},
-                    {"role": "user", "content":`Hi ${char} ${question}`}
-                ]
-            });
-
-            resolve(response.data.choices[0].message.content);
-
-        } catch (error) {
-            console.error(error);
-            reject(error);
-        }
-    });
+    
 };
 
 module.exports = chat;
--- record.js
+++ record.js
@@ -1,33 +1,11 @@
-const fs = require('fs');
-const recorder = require('node-record-lpcm16')
-const wav = require('wav')
 
-let recording = null
-let file = null
 
 const startRecording = () => {
-    console.log('Start Recording');
-    file = fs.createWriteStream('output.wav',{encoding: 'binary'})
-    const writer = new wav.Writer({
-        sampleRate: 16000,
-        channels: 1,
-    })
-    recording = recorder.record({
-        sampleRate: 16000,
-        channels: 1,
-    })
-    .stream()
-    .pipe(writer)
-    .pipe(file)
+
 };
 
 const stopRecording = () => {
-    console.log('Stop Recording')
-    if (recording) {
-        recording.end()
-        recording = null
-        file.end()    
-    }
+
 };
 
 module.exports = { 
